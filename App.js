dbf48601ff557854f1b3cef0a58cb89800f0a487
Rohan G <rohan.gupta2285@gmail.com>
2020-04-08 22:21:50 -0400

    Attempted add location

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MainScreen from './lib/screens/mainScreen';
+//import MainScreen from './lib/screens/mainScreen';
+import MainScreen from './lib/screens/mainScreen'
 
 export default App = () => (
   <MainScreen/>
